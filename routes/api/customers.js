2396440b6e7c9861a930ca6da1e9aa04daf8512b
arshaq2001 <mjmarshaq@gmail.com>
2024-02-13 17:12:10 +0530

    customer register check

--- routes/api/customers.js
+++ routes/api/customers.js
@@ -1,9 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route GET api/customers
-// @desc Test route
+// @route POST api/customers
+// @desc Register customer
 // @access Public
-router.get('/', (req, res) => res.send('Customer route'));
+router.post(
+  "/",
+  [
+    check("name", "Name is required!").not().isEmpty(),
+    check("email", "Please include valid email!").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters!"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Customer route");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // Connect MongoDB
 connectDB();
 
-app.get('/', (req, res) => res.send('API running'));
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API running"));
 
 // Define Routes
-app.use('/api/customers', require('./routes/api/customers'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/products', require('./routes/api/products'));
-app.use('/api/profiles', require('./routes/api/profiles'));
-app.use('/api/inquiries', require('./routes/api/inquiries'));
+app.use("/api/customers", require("./routes/api/customers"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/products", require("./routes/api/products"));
+app.use("/api/profiles", require("./routes/api/profiles"));
+app.use("/api/inquiries", require("./routes/api/inquiries"));
 
 const PORT = process.env.PORT || 5000;
 
