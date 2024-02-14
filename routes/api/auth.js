2ede1d72439ea0719c049b217a31c10621ae3689
arshaq2001 <mjmarshaq@gmail.com>
2024-02-14 16:59:52 +0530

    get customer by ID using middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Customer = require("../../models/Customer");
 
 // @route GET api/auth
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get("/", auth, async (req, res) => {
+  try {
+    const customer = await Customer.findById(req.customer.id).select(
+      "-password"
+    );
+    res.json(customer);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error!!!");
+  }
+});
 
 module.exports = router;
