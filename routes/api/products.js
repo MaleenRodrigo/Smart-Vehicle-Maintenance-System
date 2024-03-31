23b238fe41c5ab27353e2f691edbed0bf2ef6240
Chamikara Bandara <chamikara38@gmail.com>
2024-03-31 14:42:21 +0530

    Create product

--- routes/api/products.js
+++ routes/api/products.js
@@ -9,6 +9,34 @@ const Product = require("../../models/product");
 // @route GET api/products
 // @desc Test route
 // @access Public
+
+http://localhost:8070/product/add
+
+router.route("/add").post((req,res)=>{
+    const name = req.body.name;
+    const brand = req.body.brand;
+    const model = req.body.model;
+    const description = req.body.description;
+    const price = Number(req.body.price);
+    const stock = Number(req.body.stock);
+
+
+    const newProduct = new Product({
+        name,
+        brand,
+        model,
+        description,
+        price,
+        stock
+    })
+    newStudent.save().then(()=>{
+        res.json("Product Added Successfully !")
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+
 router.get('/', (req, res) => res.send('Products route'));
 [
     check("name", "Product name is required!").not().isEmpty(),
