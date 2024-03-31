8f718dac49da9703d2d5118124ee1c15b38797cd
Chamikara Bandara <chamikara38@gmail.com>
2024-03-31 11:43:16 +0530

    Creating products.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const Product = require("../../models/product");
 
 // @route GET api/products
 // @desc Test route
