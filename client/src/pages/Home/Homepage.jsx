491e4f96d0302eee1a5eab004462852b779ee744
arshaq2001 <mjmarshaq@gmail.com>
2024-04-07 13:43:34 +0530

    feat: inquiry api endpoint - create

--- client/src/pages/Home/Homepage.jsx
+++ client/src/pages/Home/Homepage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Homepage = () => {
     return (
         <>
-            <section className="bg-whit dark:bg-gray-900 h-screen bg-gray-200 w-full grid place-items-center">
+            <section className="bg-white dark:bg-gray-900 h-screen w-full grid place-items-center">
                 <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
                     <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
                         <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">Check this out! See what's new</span>
--- routes/api/inquiries.js
+++ routes/api/inquiries.js
@@ -1,9 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+// Models
+const Customer = require("../../models/Customer");
+const Inquiry = require("../../models/Inquiry");
+
+// @route POST api/inquiries
+// @desc Create an inquiry
+// @access Public
+router.post("/", [
+  auth,
+  [
+    check("phone", "Phone is required!").not().isEmpty(),
+    check("title", "Title is required!").not().isEmpty(),
+    check("description", "Description is required!").not().isEmpty(),
+    check("inquiryType", "Inquiry Type is required!").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      // Getting the user who creates the inquiry
+      const customer = await Customer.findById(req.customer.id).select("-password");
+
+      // Creating an new inquiry
+      const newInquiry = new Inquiry({
+        email: customer.email,
+        phone: req.body.phone,
+        title: req.body.title,
+        description: req.body.description,
+        inquiryType: req.body.inquiryType,
+        status: "Pending",
+      });
+
+      // Save the inquiry in DB and set that inquiry in variable
+      const inquiry = await newInquiry.save();
+
+      // Get the saved inquiry as response
+      res.json(inquiry);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error!");
+    }
+  },
+]);
 
 // @route GET api/inquiries
 // @desc Test route
 // @access Public
-router.get('/', (req, res) => res.send('Inquiries route'));
+router.get("/", (req, res) => res.send("Inquiries route success"));
 
 module.exports = router;
