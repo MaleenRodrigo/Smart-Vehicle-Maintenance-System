2891ac59bdc6b96f629872bd32de802571cdf95d
arshaq2001 <mjmarshaq@gmail.com>
2024-04-07 20:44:19 +0530

    feat: inquiry end point - update inquiry by id

--- routes/api/inquiries.js
+++ routes/api/inquiries.js
@@ -87,6 +87,48 @@ router.get("/:id", auth, async (req, res) => {
   }
 });
 
+// @route PUT api/inquiries/:id
+// @desc Update an inquiry
+// @access Private
+router.put("/:id", [
+  auth,
+  [
+    check("phone", "Phone is required!").not().isEmpty(),
+    check("title", "Title is required!").not().isEmpty(),
+    check("description", "Description is required!").not().isEmpty(),
+    check("inquiryType", "Inquiry Type is required!").not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const id = req.params.id;
+
+    try {
+      const inquiry = await Inquiry.findByIdAndUpdate(id, req.body);
+        
+      if (!inquiry) {
+        return res.status(400).json({ msg: "Inquiry Not Found!" });
+      }
+
+      // Getting the user who updates the inquiry
+      const customer = await Customer.findById(req.customer.id).select(
+        "-password"
+      );
+
+      const updatedInquiry = await Inquiry.findById(id);
+
+      // Get the updated inquiry as response
+      res.json(updatedInquiry);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error!");
+    }
+  },
+]);
 
 // @route   DELETE api/inquiries/:id
 // @desc    Delete an inquiry
