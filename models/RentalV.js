f3b79ebcbfcc93db001aae858166b75140ac744d
Kasun Hettiarachchi <kasunh1411@gmail.com>
2024-04-25 02:58:25 +0530

    Report Generation Page created

--- models/RentalV.js
+++ models/RentalV.js
@@ -1,42 +1,41 @@
 const mongoose = require("mongoose");
 
 const RentalSchema = new mongoose.Schema({
+  vehiclecategory: {
+    type: String,
+    required: true,
+  },
+  vehiclemodel: {
+    type: String,
+    required: true,
+  },
+  numberplate: {
+    type: String,
+    required: true,
+  },
 
-    vehiclecategory : {
-        type : String,
-        required : true
-    },
-    vehiclemodel : {
-        type : String,
-        required : true
-    },
-    numberplate : {
-        type : String,
-        required : true
-    },
-    
-    passengersno: {
-        type : Number,
-        required : true
-    },
-    condition: {
-        type : String,
-        required : true
-    },
-    distance: {
-        type : String,
-        required: true
-    },
-    additionaldetails: {
-        type : String,
-        
-    },
-    
-    
+  passengersno: {
+    type: Number,
+    required: true,
+  },
+  condition: {
+    type: String,
+    required: true,
+  },
+  distance: {
+    type: String,
+    required: true,
+  },
+  additionaldetails: {
+    type: String,
+  },
+
+  //Added by Kaizma
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = Rental = mongoose.model("rental", RentalSchema);
-
-
-
-
