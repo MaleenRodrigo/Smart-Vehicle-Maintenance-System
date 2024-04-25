30a935fdbcc9842a828f0ff6384a6766e693ec02
arshaq2001 <mjmarshaq@gmail.com>
2024-04-25 20:27:51 +0530

    fix: profile page ticket alert dialog added when deleting

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -11,6 +11,13 @@ const token = localStorage.getItem("token");
 // console.log("profileToken=> ", token);
 const Profile = () => {
   const navigate = useNavigate();
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [currentInquiryId, setCurrentInquiryId] = useState(null);
+
+  const toggleModal = (id) => {
+    setCurrentInquiryId(id);
+    setModalOpen(!isModalOpen);
+  };
 
   const [inquiries, setInquiries] = useState([]);
 
@@ -32,7 +39,7 @@ const Profile = () => {
     try {
       await deleteInquiry(id, token);
       setInquiries((currentInquiries) =>
-        currentInquiries.filter((inquiry) => inquiry.id !== id)
+        currentInquiries.filter((inquiry) => inquiry._id !== id)
       );
       // navigate("/profile");
       // console.log(response.data);
@@ -147,17 +154,17 @@ const Profile = () => {
                   <td className="px-2 py-4 text-right">
                     <a
                       // href="#"
-                      className="font-medium text-blue-600 :text-blue-500 hover:underline"
-                      onClick={() => handleDeleteInquiries()}
+                      className="font-medium text-blue-600 :text-blue-500 cursor-pointer"
+                      // onClick={() => handleDeleteInquiries()}
                     >
                       <EditIcon />
                     </a>
                   </td>
                   <td className="px-2 py-4 text-right">
                     <a
-                      // href="#"
-                      onClick={() => handleDeleteInquiries(inquiry._id)}
-                      className="font-medium text-red-600 :text-blue-500 hover:underline"
+                      // onClick={() => handleDeleteInquiries(inquiry._id)}
+                      onClick={() => toggleModal(inquiry._id)}
+                      className="font-medium text-red-600 :text-blue-500 cursor-pointer"
                     >
                       <DeleteIcon />
                     </a>
@@ -172,6 +179,80 @@ const Profile = () => {
           </tbody>
         </table>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+
+      {isModalOpen && (
+        <div
+          id="popup-modal"
+          tabIndex="-1"
+          className="fixed inset-0 z-50 overflow-y-auto overflow-x-hidden flex justify-start items-end w-full h-[calc(100%-1rem)]"
+        >
+          <div className="relative p-4 w-full max-w-md">
+            <div className="relative bg-gray-200 rounded-lg shadow :bg-gray-700">
+              <button
+                onClick={toggleModal}
+                type="button"
+                className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center :hover:bg-gray-600 :hover:text-white"
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
+                </svg>
+                <span className="sr-only">Close modal</span>
+              </button>
+              <div className="p-4 md:p-5 text-center">
+                <svg
+                  className="mx-auto mb-4 text-gray-400 w-12 h-12 :text-gray-200"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M10 11V6m0 8h.01M19 10a9 9 0 11-18 0 9 9 0 0118 0Z"
+                  />
+                </svg>
+                <h3 className="mb-5 text-lg font-normal text-gray-500 :text-gray-400">
+                  Are you sure you want to delete this product?
+                </h3>
+                <button
+                  onClick={() => {
+                    handleDeleteInquiries(currentInquiryId);
+                    toggleModal(false);
+                  }}
+                  className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 :focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                >
+                  Yes, I'm sure
+                </button>
+                <button
+                  onClick={toggleModal}
+                  className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 :focus:ring-gray-700 :bg-gray-800 :text-gray-400 :border-gray-600 :hover:text-white :hover:bg-gray-700"
+                >
+                  No, cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
