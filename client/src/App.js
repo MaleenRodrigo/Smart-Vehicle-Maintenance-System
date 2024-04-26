7701af018879792db9cfa124550f32843c7d59a6
Chamikara Bandara <chamikara38@gmail.com>
2024-04-26 14:17:45 +0530

    fix: file paths in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import { BrowserRouter as Router, Routes, Route, Form } from "react-router-dom";
 import Homepage from "./pages/Home/Homepage";
 import Inquiries from "./pages/Inquiries/Inquiries";
 import { Crisp } from "crisp-sdk-web";
-import Product from "./pages/Products/product";
 import Shop from "./pages/Shop/Shop";
+import AddProduct from "./pages/Products/Addproduct";
 import Profile from "./pages/Profile/Profile";
 import Card from "./pages/Card/Card";
 import { RentalService } from "./pages/Rentals/RentalService";
@@ -18,8 +18,10 @@ import { JeepsMain } from "./pages/Rentals/JeepsMain";
 import { CarsDetails } from "./pages/Rentals/CarsDetails";
 import { VansDetails } from "./pages/Rentals/VansDetails";
 import { JeepsDetails } from "./pages/Rentals/JeepsDetails";
+import { AdminPanel } from "./pages/Admin/Admin";
+import { AdminInquiry } from "./pages/Admin/Inquiries/AdminInquiry";
 import { VehicleRentalReport } from "./pages/Rentals/JeepsDetails";
-import UpdateInquiryForm from "./pages/Inquiries/UpdateInquiryForm";
+import UpdateInquiryForm from "./pages/Admin/Inquiries/UpdateInquiryForm";
 
 function App() {
   useEffect(() => {
@@ -36,7 +38,6 @@ function App() {
           <Route exact path="/signup" Component={Register} />
           <Route exact path="/inquiry" Component={Inquiries} />
           <Route exact path="/inquiry/update" Component={UpdateInquiryForm} />
-          <Route exact path="/product" Component={Product} />
           <Route exact path="/profile" Component={Profile} />
           <Route exact path="/profile/card" Component={Card} />
           <Route exact path="/profile/card/:cardId" Component={Card} />
@@ -51,6 +52,8 @@ function App() {
           <Route exact path="/carsdetails" Component={CarsDetails} />
           <Route exact path="/vansdetails" Component={VansDetails} />
           <Route exact path="/jeepsdetails" Component={JeepsDetails} />
+          <Route exact path="/admin" Component={AdminPanel} />
+          <Route exact path="/admin/inquiries" Component={AdminInquiry} />
         </Routes>
       </>
     </Router>
