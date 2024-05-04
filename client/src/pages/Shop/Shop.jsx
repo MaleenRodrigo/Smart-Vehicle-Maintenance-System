a0d31793d0b35c983f11a9d7e890fa9e9bc4c79d
Randini Maliksha <maliksharandini@gmail.com>
2024-05-04 19:19:53 +0530

    fix: conflicts in shop

--- client/src/pages/Shop/Shop.jsx
+++ client/src/pages/Shop/Shop.jsx
@@ -183,46 +183,6 @@ function Shop() {
                         )}
                       </div>
                     ))}
-                      <div>
-                        <p className="text-lg font-bold text-gray-900 dark:text-white">
-                          {" "}
-                          Rs. {product.price}{" "}
-                        </p>
-                        <p className="text-lg font-bold text-gray-900 light:text-white">
-                          {" "}
-                          Rs. {product.price}{" "}
-                        </p>
-                      </div>
-                      <div className="mt-6 flex items-center gap-2.5">
-                        <button
-                          type="button"
-                          onClick={() => {
-                            addToCart(product);
-                          }}
-                          className="inline-flex w-full items-center justify-center rounded-lg bg-primary px-5 py-2.5 text-sm font-medium  text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 :bg-primary-600 :hover:bg-primary-700 :focus:ring-primary-800"
-                        >
-                          <svg
-                            className="-ms-2 me-2 h-5 w-5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              stroke="currentColor"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7h-1M8 7h-.688M13 5v4m-2-2h4"
-                            />
-                          </svg>
-                          Add to cart
-                        </button>
-                      </div>
-                    </div>
-                  ))}
                 </div>
               </div>
             </div>
