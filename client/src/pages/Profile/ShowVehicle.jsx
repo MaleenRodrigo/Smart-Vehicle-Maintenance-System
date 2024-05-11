e413fe7839d93b90912ac92289385ba10ceace91
Maleen Rodrigo <maleenrodrigo05@gmail.com>
2024-05-11 05:50:22 +0530

    Fix Minor Changes

--- client/src/pages/Profile/ShowVehicle.jsx
+++ client/src/pages/Profile/ShowVehicle.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ApiHelper from "../../helper/apiHelper";
 import { Link } from "react-router-dom";
+import jsPDF from 'jspdf';
 import Navbar from "../../components/NavbarAfter";
 import Footer from "../../components/Footer";
 
@@ -9,35 +10,46 @@ const api = new ApiHelper(); // Initialize an instance of ApiHelper
 const ShowVehicle = () => {
   const [vehicles, setVehicles] = useState([]);
   const [remainingTime, setRemainingTime] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(""); 
+  const [isResponsiveSearch, setIsResponsiveSearch] = useState(false); 
 
-  const [searchTerm, setSearchTerm] = useState(""); // State variable for the search term
-  const [isResponsiveSearch, setIsResponsiveSearch] = useState(false); // State variable for responsive search box
+  const [confirmDelete, setConfirmDelete] = useState(false); 
+  const [vehicleToDelete, setVehicleToDelete] = useState(null);
 
   useEffect(() => {
     const fetchVehicles = async () => {
       try {
-        // Fetch vehicles using the ApiHelper get method
-        const token = localStorage.getItem("token"); // Assuming you store the token in localStorage
+        const token = localStorage.getItem("token");
         const data = await api.get("profile/vehicles", {}, token);
-        setVehicles(data); // Update the vehicles state with the fetched data
-        
-
+        setVehicles(data);
       } catch (error) {
         console.error("Error fetching vehicles:", error.message);
       }
     };
 
-    fetchVehicles(); // Call the fetchVehicles function when the component mounts
-  }, []); // Empty dependency array ensures the effect runs only once
+    fetchVehicles();
+  }, []);
 
   const handleDelete = async (vehicleId) => {
+    // Store the vehicle ID to delete in state and show the confirmation modal
+    setVehicleToDelete(vehicleId);
+    setConfirmDelete(true);
+  };
+
+  const handleConfirmDelete = async () => {
     try {
       const token = localStorage.getItem("token");
-      await api.delete(`profile/vehicle/${vehicleId}`, token);
-      setVehicles(vehicles.filter((vehicle) => vehicle._id !== vehicleId));
+      await api.delete(`profile/vehicle/${vehicleToDelete}`, token);
+      setVehicles(vehicles.filter((vehicle) => vehicle._id !== vehicleToDelete));
+      setConfirmDelete(false); // Hide the confirmation modal after deletion
     } catch (error) {
       console.error("Error deleting vehicle:", error.message);
     }
+  };
+
+  const handleCancelDelete = () => {
+    // Hide the confirmation modal
+    setConfirmDelete(false);
   };
 
   // Filter vehicles based on the search term
@@ -52,6 +64,82 @@ const ShowVehicle = () => {
     const days = Math.ceil(difference / (1000 * 60 * 60 * 24));
     return days;
 };
+function formatDateToISO(dateString) {
+  // Create a new Date object from the provided date string
+  const date = new Date(dateString);
+
+  // Check if the date is valid
+  if (isNaN(date.getTime())) {
+      // If the date is invalid, return null or throw an error
+      return null;
+  } else {
+      // Format the date as "YYYY-MM-DD"
+      const formattedDate = date.toISOString().split('T')[0];
+      return formattedDate;
+  }
+}
+
+
+const generateReportPDF = () => {
+  const doc = new jsPDF();
+
+  doc.text("Vehicles Report", 105, 10, { align: 'center' }); // Center the title horizontally
+
+  let startY = 20;
+  doc.setFontSize(12);
+
+  // Table headers
+  const headers = [
+    "Make",
+    "Model",
+    "Year",
+    "Registration Number",
+    "Fuel Type",
+    "Daily Usage",
+    "License Number",
+    "Insurance Number",
+    "Insurance Type"
+  ];
+
+  const data = filteredVehicles.map(({ make, model, year, registrationnumber, fueltype, dailyusage, licensenumber, insurancenumber, insurancetype }) => [
+    make,
+    model,
+    year,
+    registrationnumber,
+    fueltype,
+    dailyusage,
+    licensenumber,
+    insurancenumber,
+    insurancetype
+  ]);
+
+  doc.autoTable({
+    startY,
+    head: [headers],
+    body: data,
+    theme: 'grid',
+    styles: {
+      fontSize: 10,
+      overflow: 'linebreak',
+    },
+    columnStyles: {
+      0: { cellWidth: 15 },
+      1: { cellWidth: 20 },
+      2: { cellWidth: 15 },
+      3: { cellWidth: 25 },
+      4: { cellWidth: 20 },
+      5: { cellWidth: 20 },
+      6: { cellWidth: 20 },
+      7: { cellWidth: 25 },
+      8: { cellWidth: 25 }
+      
+    },
+  });
+
+  doc.save("vehicles_report.pdf");
+};
+
+
   return (
     <>
     <Navbar />
@@ -59,7 +147,8 @@ const ShowVehicle = () => {
       <h1 className="mt-28 text-2xl font-bold mb-4">Vehicles</h1>
 
       
-      {/* <div className="relative w-full max-w-md">
+      
+      <div className="relative w-full max-w-md">
         <input
           type="text"
           placeholder="Search by make"
@@ -74,7 +163,7 @@ const ShowVehicle = () => {
             
           </div>
         )}
-      </div> */}
+      </div>
 
 
       {filteredVehicles.length === 0 ? (
@@ -93,7 +182,7 @@ const ShowVehicle = () => {
 
                   <span class="mt-1 text-sm leading-7 text-gray-700 sm:col-span-2 sm:mt-0">Fuel Type : {vehicle.fueltype}</span><br/>
 
-                  <span class="mt-1 text-sm leading-7 text-gray-700 sm:col-span-2 sm:mt-0">Average Daily Usage : {vehicle.dailyusage}</span><br/><br/>
+                  <span class="mt-1 text-sm leading-7 text-gray-700 sm:col-span-2 sm:mt-0">Average Daily Usage : {vehicle.dailyusage} Km</span><br/><br/>
 
                   <span class="mt-1 text-sm leading-7 sm:col-span-2 sm:mt-0" style={{ color: calculateRemainingTime(vehicle.licenseexpiry) < 30 ? 'red' : 'inherit', fontWeight: calculateRemainingTime(vehicle.licenseexpiry) < 30 ? 'bold' : 'normal' }}>License Expires in: {calculateRemainingTime(vehicle.licenseexpiry)} days</span><br/>
 
@@ -125,7 +214,45 @@ const ShowVehicle = () => {
           Go back
         </button>
       </Link>
+
+      <button
+            onClick={generateReportPDF}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4">
+            Download Report
+          </button>
     </div>
+
+    {confirmDelete && (
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen px-4 text-center">
+            <div className="fixed inset-0 transition-opacity">
+              <div className="absolute inset-0 bg-black opacity-50"></div>
+            </div>
+            <div className="relative z-10 p-6 bg-white shadow-md rounded-xl">
+              <p className="mb-4">
+                Are you sure you want to delete this vehicle?
+              </p>
+              <div className="flex justify-center">
+                <button
+                  className="mr-2 px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none"
+                  onClick={handleConfirmDelete} 
+                >
+                  Delete
+                </button>
+                <button
+                  className="px-4 py-2 text-sm font-medium text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 focus:outline-none"
+                  onClick={handleCancelDelete} 
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    )}
+
+
+    <Footer />
     </>
   );
 };
--- client/src/pages/Profile/UpdateVehicle.jsx
+++ client/src/pages/Profile/UpdateVehicle.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from "../../components/NavbarAfter";
 import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useParams } from 'react-router-dom'; 
 import axios from 'axios'; 
 
 const VehicleDetails = () => {
+  const navigate = useNavigate();
   const { v_id } = useParams(); // Get the vehicle ID from the URL params
   const [vehicle, setVehicle] = useState(null); // State to store the vehicle details
   const token = localStorage.getItem("token");
@@ -65,32 +68,321 @@ const VehicleDetails = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Send a PUT request to update the vehicle details
       const res = await axios.put(`/api/profile/vehicle/${v_id}`, formData,{
         headers: {
           "x-auth-token": token,
         },
       });
       console.log(res.data); // Log the updated vehicle details
+      navigate("/ShowVehicle");
     } catch (err) {
       console.error(err.response.data);
     }
   };
+
+  function formatDateToISO(dateString) {
+    // Create a new Date object from the provided date string
+    const date = new Date(dateString);
+
+    // Check if the date is valid
+    if (isNaN(date.getTime())) {
+        // If the date is invalid, return null or throw an error
+        return null;
+    } else {
+        // Format the date as "YYYY-MM-DD"
+        const formattedDate = date.toISOString().split('T')[0];
+        return formattedDate;
+    }
+}
 
   if (!vehicle) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h2>Edit Vehicle Details</h2>
-      <form onSubmit={handleSubmit}>
-        {/* Input fields for editing vehicle details */}
-        <input type="text" name="make" value={formData.make} onChange={handleChange} />
-        {/* Other input fields go here */}
-        <button type="submit">Save Changes</button>
-      </form>
+    <><Navbar />
+
+    <br />
+
+    <div className="text-center">
+      <h3 className="text-3xl font-semibold leading-7 text-gray-900 mt-28 mb-10">
+        Update Vehicle
+      </h3>
     </div>
+
+
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+        
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="make"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Make:
+      </label>
+      <input
+        type="text"
+        id="make"
+        name="make"
+        value={formData.make}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+        required
+      />
+    </div>
+
+    <div class="grid md:grid-cols-2 md:gap-6">
+
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="model"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Model:
+      </label>
+      <input
+        type="text"
+        id="model"
+        name="model"
+        value={formData.model}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="year"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Year:
+      </label>
+      <input
+        type="text"
+        id="year"
+        name="year"
+        value={formData.year}
+        onChange={handleChange}
+        pattern="\d{4}" title="Please enter a valid year (4 digits)"
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+    </div>
+
+    
+
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="registrationnumber"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Registration Number:
+      </label>
+      <input
+        type="text"
+        id="registrationnumber"
+        name="registrationnumber"
+        value={formData.registrationnumber}
+        onChange={handleChange}
+        pattern="[a-zA-Z]{3}-\d{4}|[a-zA-Z]{2}-\d{4}|\d{2}-\d{3}|\d{3}-\d{3}" title="Please enter a valid registration number"
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+
+    <div class="grid md:grid-cols-2 md:gap-6">
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="fueltype"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Fuel Type:
+      </label>
+      <select
+        id="fueltype"
+        name="fueltype"
+        value={formData.fueltype}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      >
+        <option value="">Select Fuel Type</option>
+        <option value="Gasoline">Petrol</option>
+        <option value="Diesel">Diesel</option>
+        <option value="Electric">Hybrid</option>
+        <option value="Electric">Electric</option>
+      </select>
+    </div>
+
+    
+
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="dailyusage"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Daily Usage in KM:
+      </label>
+      <input
+        type="number"
+        id="dailyusage"
+        name="dailyusage"
+        value={formData.dailyusage}
+        onChange={handleChange}
+        max="200"
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+    </div>
+
+    <div className="relative z-0 w-full mb-5 mt-12 group">
+      <label
+        htmlFor="licensenumber"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        License Number:
+      </label>
+      <input
+        type="text"
+        id="licensenumber"
+        name="licensenumber"
+        value={formData.licensenumber}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+
+    <div class="grid md:grid-cols-2 md:gap-6">
+
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="licenseissued"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        License Issued Date:
+      </label>
+      <input
+        type="date"
+        id="licenseissued"
+        name="licenseissued"
+        value={formatDateToISO(formData.licenseissued)}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="licenseexpiry"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        License Expiry Date:
+      </label>
+      <input
+        type="date"
+        id="licenseexpiry"
+        name="licenseexpiry"
+        value={formatDateToISO(formData.licenseexpiry)}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+    </div>
+
+    <div className="relative z-0 w-full mb-5 mt-12 group">
+      <label
+        htmlFor="insurancenumber"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Insurance Number:
+      </label>
+      <input
+        type="text"
+        id="insurancenumber"
+        name="insurancenumber"
+        value={formData.insurancenumber}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="insurancetype"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Insurance Type:
+      </label>
+      <input
+        type="text"
+        id="insurancetype"
+        name="insurancetype"
+        value={formData.insurancetype}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+
+    <div class="grid md:grid-cols-2 md:gap-6">
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="issueddate"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Insurance Issued Date:
+      </label>
+      <input
+        type="date"
+        id="issueddate"
+        name="issueddate"
+        value={formatDateToISO(formData.issueddate)}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+    
+    <div className="relative z-0 w-full mb-5 group">
+      <label
+        htmlFor="expirydate"
+        className="peer-focus:font-medium absolute text-xl text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+      >
+        Insurance Expiry Date:
+      </label>
+      <input
+        type="date"
+        id="expirydate"
+        name="expirydate"
+        value={formatDateToISO(formData.expirydate)}
+        onChange={handleChange}
+        className="block py-2.5 px-0 w-full text-xm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+        required
+      />
+    </div>
+    </div>
+    <button
+      type="submit"
+      className="bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 block mx-auto mt-8 mb-4">
+      Save Changes
+    </button>
+
+    <Link to="/ShowVehicle" className="block ">
+      <button className="border border-gray-300 px-4 py-2 rounded-md hover:bg-gray-100  block mx-auto mb-16">
+        Go back
+      </button>
+    </Link> 
+
+  </form>
+  <Footer />
+  </>
   );
 };
 
